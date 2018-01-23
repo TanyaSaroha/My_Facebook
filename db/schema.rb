79162e509652ce24876d87e3e7fb705f22eb1729
Tanya Saroha <tanya@joshsoftware.com>
2018-01-23 15:39:03 +0530

    Validations added to Address model and Schema generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123071603) do
+ActiveRecord::Schema.define(version: 20180123081203) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "area"
+    t.string "city"
+    t.string "state"
+    t.string "pin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+>>>>>>> 0ec5b97... Validations added to Address model and Schema generated
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
