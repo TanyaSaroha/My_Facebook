18a522a576f9cb1186f529649e3316ebe2912299
Tanya Saroha <tanya@joshsoftware.com>
2018-01-23 15:41:29 +0530

    Created Model Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123081203) do
+ActiveRecord::Schema.define(version: 20180123083222) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "area"
     t.string "city"
     t.string "state"
     t.string "pin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
