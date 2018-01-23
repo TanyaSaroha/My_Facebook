3cf79ae5003d7cdb311b31aca738ace51009dd58
Tanya Saroha <tanya@joshsoftware.com>
2018-01-23 15:45:25 +0530

    Revert "Revert "User""
    This reverts commit 996705d42f28a6aeb29a9c63eb79e6b7bc733dc2.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	validates :email, presence: true, uniqueness:true, format: { with: /\A[A-Za-z0-9\W]+*@[A-Za-z0-9\W]+[.][a-z]+\z/, message:'Invlid mail' }
+	validates :full_name, presence: true, format: { with: /\A[A-Za-z]* [A-Za-z]*+\z/, message: 'Full name'}
+	validates :password, presence: true, format: { with: /(?=.*[a-z])(?=.*[A-z])(?=.*[0-9])(?=.*\W)/, message: 'Not strong password' }
+	validates :dob, presence: true
+	validates :gender, presence: true, format: { with: /\AMale\z|\AFemale\z/, message: 'Only Female/Male allowed' }
+end
